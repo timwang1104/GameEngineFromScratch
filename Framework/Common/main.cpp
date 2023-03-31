18fec6b7be0eba3d5c0160583a4a4449e2c13f36
tywang <timwang1104@126.com>
2023-03-31 15:53:55 +0800

    update to article_6

--- Framework/Common/main.cpp
+++ Framework/Common/main.cpp
@@ -8,17 +8,13 @@ namespace My {
 
 int main(int argc, char** argv)
 {
-    printf("hello main\n");
-    // int ret;
-    // if((ret = g_pApp->Initialize()) != 0)    {
-    //     printf("App Initialize failed, will exit now.");
-    //     return ret;
-    // }
+    int ret;
+    if((ret = g_pApp->Initialize()) != 0)    {
+        return ret;
+    }
 
-    // while(!g_pApp->IsQuit()) {
-    //     printf("main loop\n");
-    //     g_pApp->Tick();
-    // }
-
+    while(!g_pApp->IsQuit()) {
+        g_pApp->Tick();
+    }
     return 0;
 }
